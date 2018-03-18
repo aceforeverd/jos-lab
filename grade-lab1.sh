0bff4c34567196295655b85c5594c0503ee60afd
Yelly <yu000013@163.com>
2018-03-17 22:30:19 -0700

    deleted %n exercises

--- grade-lab1.sh
+++ grade-lab1.sh
@@ -35,14 +35,6 @@ score=0
 		echo WRONG $time
 	fi
     
-    if grep "error! writing through NULL pointer! (%n argument)" jos.out >/dev/null && grep "warning! The value %n argument pointed to has been overflowed!" jos.out > /dev/null && grep "chnum1: 29 chnum2: 30" jos.out > /dev/null && grep "chnum1: -1" jos.out > /dev/null
-    then 
-        score=`expr 10 + $score`
-        echo OK $time
-    else
-        echo WRONG $time
-    fi
-
 	echo_n "Backtrace: "
 	args=`grep -a "eip f0100.* ebp f01.* args" jos.out | awk '{ print $6 }'`
 	cnt=`echo $args | grep '^00000000 00000000 00000001 00000002 00000003 00000004 00000005' | wc -w`
@@ -74,8 +66,8 @@ END { printf("\n") }' | grep '^00000000 00000000 00000001 00000002 00000003 0000
 	fi
 
 
-echo "Score: $score/80"
+echo "Score: $score/70"
 
-if [ $score -lt 60 ]; then
+if [ $score -lt 50 ]; then
     exit 1
 fi
