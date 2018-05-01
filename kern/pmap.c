f6ea59e844b35cf0e98ef12c1f46a0b5b3d68062
aceforeverd <dace1p@gmail.com>
2018-05-01 19:44:57 +0800

    mem_init_mp()

--- kern/pmap.c
+++ kern/pmap.c
@@ -290,7 +290,9 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
-
+    for (int i = 0; i < NCPU; i++) {
+        boot_map_region(kern_pgdir, KSTACKTOP - i * (KSTKSIZE + KSTKGAP) - KSTKSIZE, KSTKSIZE, (physaddr_t) (percpu_kstacks + i), PTE_W);
+    }
 }
 
 // --------------------------------------------------------------
