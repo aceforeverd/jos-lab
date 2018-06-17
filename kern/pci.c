04dc3ad47ef837b9cc6fe4b208ed8ccb1bc508a6
aceforeverd <dace1p@gmail.com>
2018-06-17 20:06:57 +0800

    fix pic_attach_vendor

--- kern/pci.c
+++ kern/pci.c
@@ -30,7 +30,7 @@ struct pci_driver pci_attach_class[] = {
 
 // pci_attach_vendor matches the vendor ID and device ID of a PCI device
 struct pci_driver pci_attach_vendor[] = {
-    { 8086, 100, e1000_attach},
+    { 0x8086, 0x100e, e1000_attach},
 	{ 0, 0, 0 },
 };
 
