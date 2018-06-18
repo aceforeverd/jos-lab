37a15129f1d0f12a784b41371ded525a485c825e
aceforeverd <dace1p@gmail.com>
2018-06-18 17:08:31 +0800

    bug fix

--- kern/e1000.c
+++ kern/e1000.c
@@ -13,7 +13,7 @@
 int e1000_attach(struct pci_func *pcif) {
 #ifdef E1000_DEBUG
     cprintf("e1000_attach: \n");
-    cprintf("reg_base[0]: %8x, reg_size[0]: %8x\n", pcif->reg_base[0], reg_size[0]);
+    cprintf("reg_base[0]: %8x, reg_size[0]: %8x\n", pcif->reg_base[0], pcif->reg_size[0]);
 #endif
     pcif->reg_base[0] = E1000_START;
     pcif->reg_size[0] = E1000_SIZE;
@@ -21,7 +21,7 @@ int e1000_attach(struct pci_func *pcif) {
     uintptr_t addr_base = pcif->reg_base[0];
     int r;
     for (uintptr_t off = 0; off < pcif->reg_size[0]; off += PGSIZE) {
-        if ((r = sys_page_alloc(0, addr_base + off, PTE_PCD | PTE_PWT)) < 0) {
+        if ((r = sys_page_alloc(0, (void *) (addr_base + off), PTE_PCD | PTE_PWT)) < 0) {
             panic("sys_page_alloc: %e", r);
         }
     }
